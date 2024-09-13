72d6f35e666ad85f564c596024b50656da320f4b
Zharfan Zakkiyan <zharfanpintar@gmail.com>
2024-09-13 20:41:31 +0700

    pts-kelompok-8

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ module.exports = {
   content: ["./dist/*.{html,js}", "./src/**/*.{html,js,css}"],
   theme: {
     extend: {
+      backgroundImage:{
+        'element-3d':"url(/src/img/image.png)",
+      },
       colors : {
         'bg-main' : '#00006b',
         'white': '#ffffff',
+        'white-grayish' : '#F4F4F4',
       },
   
     fontFamily : {
