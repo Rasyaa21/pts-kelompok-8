4920fdc910b1df5d41c19ca32383c4ca64866f59
Rasyaa21 <naufalrasya21907@gmail.com>
2024-09-14 17:45:04 +0700

    landing done

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,8 @@ console.log('JavaScript file is loaded');
 const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 
+console.log("js init")
+
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('hidden');
 });
